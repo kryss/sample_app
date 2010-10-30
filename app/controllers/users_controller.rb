d605a4218b4b1e0a1e2ca67ae6478ccdcc9cb397
Kryss <kryss.col@gmail.com>
2010-10-30 02:54:05 +0200

    End

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
  
   before_filter :signed_user, :only => [:new, :create] 
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate, :except => [:show, :new, :create]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
   
@@ -58,6 +58,21 @@ class UsersController < ApplicationController
     flash[:success] = "User destroyed."
     redirect_to users_path
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
   
   private
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,14 @@ class User < ActiveRecord::Base
     attr_accessible :name, :email, :password, :password_confirmation
     
     has_many :microposts, :dependent => :destroy
+    has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+    has_many :following, :through => :relationships, :source => :followed
+    has_many :reverse_relationships, :foreign_key => "followed_id",
+                                     :class_name => "Relationship",
+                                     :dependent => :destroy
+    has_many :followers, :through => :reverse_relationships, :source => :follower
+
     
     email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -39,8 +47,9 @@ class User < ActiveRecord::Base
     end
   
     def feed
-      Micropost.where("user_id = ?", id)
+      Micropost.from_users_followed_by(self)
     end
+
     
     def self.authenticate(email, submitted_password)
       user = find_by_email(email)
@@ -52,6 +61,18 @@ class User < ActiveRecord::Base
       (user && user.salt == cookie_salt) ? user : nil
     end
     
+    def following?(followed)
+      relationships.find_by_followed_id(followed)
+    end
+  
+    def follow!(followed)
+      relationships.create!(:followed_id => followed.id)
+    end
+
+    def unfollow!(followed)
+      relationships.find_by_followed_id(followed).destroy
+    end
+
     private
       def encrypt_password
         self.salt = make_salt if new_record?
